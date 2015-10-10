0cb58770a22e27437f3ea8349d48b12f224cf26b
van.noid <547313481@qq.com>
2015-10-10 13:15:05 +0800

    a little edit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -43,6 +43,13 @@
         <div class="container">
             <div class="content">
                 <form method="get" action="hash">
+                    <p>暂时做了个tut的分支里面放些学前后端入门需要达标的效果</p>
+                    <a href="http://web.seiee.com">大神写的教程博客</a>
+                    <br>
+                    <br>
+                    <a href="tutorial">前端的要求页面</a>
+                    <br><br>
+                    后端的一个小程序 可以现在里面输入东西确定之后会返回一个hash值，摸索一下怎么做的<br>
                     <input name="value" placeholder="enter string here">
                     <button>get hash</button>
                 </form>
