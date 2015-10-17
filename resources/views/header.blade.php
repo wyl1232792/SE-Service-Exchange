13a1b0dd1f739dec67608fb9c8acdbc75eba17e5
zcwmyself <zcw@zci.local>
2015-10-17 23:32:38 +0800

    User Info Update

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -11,13 +11,10 @@
           </button>
           <a class="navbar-brand" href="#">Exchange Platform</a>
       </div>
-
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
         <li class="active"><a href="#">Front page<span class="sr-only">(current)</span></a></li>
-          <li><a href="#">Info</a></li>
-          <li><a href="#">Info</a></li>
           <li><a href="#">Info</a></li>
         <li class="dropdown">
           <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">info
@@ -33,15 +30,22 @@
           </ul>
         </li>
       </ul>
-
-      <form class="navbar-form navbar-left" role="search">
-        <div class="form-group">
-          <input type="text" class="form-control" placeholder="Search">
-        </div>
-        <button type="submit" class="btn btn-default">Search</button>
-      </form>
-
-       <ul class="nav navbar-nav navbar-right"></ul>
+      <!-- User Info-->
+       <ul class="nav navbar-nav navbar-right">
+          <form class="navbar-form navbar-left" role="search">
+            <div class="form-group">
+              <input type="text" class="form-control" placeholder="Username">
+            </div>
+            <div class="form-group">
+              <input type="password" class="form-control" placeholder="Password">
+            </div>
+            <div class="form-group">
+              <input type="text" class="form-control" placeholder="Group">
+            </div>
+            <button type="submit" class="btn btn-default">提交</button>
+          </form> 
+      <!-->end Userinfo</!-->
+       </ul>
     </div><!-- /.navbar-collapse -->
    </div><!-- /.container-fluid -->
   </nav>
