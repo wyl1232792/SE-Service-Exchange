4ce895302972f26ac38c73bd52a5918865ff15ef
wyl1232792 <wyl1232792@hotmail.com>
2015-12-18 20:33:26 +0800

    remove tq related information

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -2,31 +2,9 @@
 	<div id="footer">
         <div class="content">
             <div class="pull-left">
-                <p>友情链接:&nbsp;<a target="_blank" href="http://zhai.me"></a></p>
-                <p>©&nbsp;2015&nbsp;Tongqu&nbsp;沪ICP备</p>
-                <a href="http://www.dongyueweb.com" target="_blank">Powered&nbsp;by&nbsp;</a>
+                <p>SE-Service-Exchange</p>
             </div>
             <div class="pull-right right-part">
-                <div>
-                    <!--QQ 客服的代码，记的换qq号！-->
-                    <a target="_blank" href="http://wpa.qq.com/msgrd?v=3&amp;uin=2575386529&amp;site=qq&amp;menu=yes"><img border="0" src="http://wpa.qq.com/pa?p=2:2575386529:41" alt="点击联系同去网客服" title="点击联系同去网客服"></a>
-                </div>
-                <!--end of QQ 客服 -->
-
-                <div style="margin:-1px -10px 0px 10px">
-                    <iframe scrolling="no" frameborder="0" allowtransparency="true" src="http://widget.renren.com/plugin/followbutton?page_id=601387858&amp;color=0&amp;model=1" style="width:150px;height:40px;"></iframe>
-                </div>
-
-                <div>
-                    <a href="http://www.tongqu.me/index.php/contact">联系我们</a>
-                </div>
-
-                <div>
-                    <a href="http://www.tongqu.me/index.php/disclaimer">免责声明</a>
-                </div>
-                <div>
-                    <a href="/index.php/app">移动端下载</a>
-                </div>
                 <div style="clear:both"></div>
             </div>
             <div style="clear:both"></div>
